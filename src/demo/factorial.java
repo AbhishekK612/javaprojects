f2af022fff3f93391f3298aedf816967b8f4749f
abhi <abhi@gmail.com>
2025-12-09 14:34:18 +0000

    updated factorial.java

--- src/demo/factorial.java
+++ src/demo/factorial.java
@@ -3,7 +3,7 @@ package demo;
 public class factorial {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		int num=4;
 		int fact=1;
 		for(int i=num;i>=1;i -- ){
