ac04e3aaa52f87bd038e7f58bd98a50373321048
Abhishek <abhishek@gmail.com>
2025-12-09 19:40:43 +0530

    commited first java projects

---
+++ src/demo/factorial.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=4;
+		int fact=1;
+		for(int i=num;i>=1;i -- ){
+		fact = fact*i;
+
+		System.out.print(fact);
+	}
+
+}
+}
